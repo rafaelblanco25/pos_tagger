ca142b961f0b01831b002ae21f93f69f2548dd7a
Rafael <rafael@MacBook-Pro-de-Rafael.local>
2019-04-14 07:43:04 +0200

    Comments

--- pos.py
+++ pos.py
@@ -4,6 +4,7 @@ def different_words(file, type_of_set):
 	set_to_count = json.load(open("./corpus/en/en."+file+"."+type_of_set+".json"))
 	count = {}					#dict with differents words
 	count_without_caps = {}		#dict with differents lowercase words
+	different_labels = []		#array with labels
 	for words, labels in set_to_count:
 		for word in words:
 			if word.lower() in count_without_caps:
@@ -16,15 +17,26 @@ def different_words(file, type_of_set):
 			else:
 				count[word] = 1
 
+		for label in labels:
+			if label not in different_labels:
+				different_labels.append(label) 
+
+
 	print("Number of differents words: ", len(count))
 	print("Number of differents lowercase words: ", len(count_without_caps))
+	print("Number of differents labels: ", len(different_labels))
 
-	return count, count_without_caps
+	return count, count_without_caps, different_labels
+
 
 class Perceptron:
-	def __init__(self, ):
+	def __init__(self, number_of_labels):
+		w = [0 for x in range(number_of_labels)] #init weights
+		loss_function_evolve = []
 
-		#bla
+	def train(self):
+
+	def dot_product(self, features):
 
 
 if __name__ == "__main__":
